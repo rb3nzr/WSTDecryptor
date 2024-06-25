4f07b4f8371d7a5828d1d82f6e9970107f5638e7
Ben <107324942+rb3nzr@users.noreply.github.com>
2024-06-25 00:17:06 -0500

    fixed logging

--- core/Utility.py
+++ core/Utility.py
@@ -1,18 +1,38 @@
 from core.Config import *
 
-from colorama import Fore
+from colorama import Back, Fore
 import subprocess
 import platform 
 import logging 
 
+class CFormatter(logging.Formatter):
+    COLORS = {
+        "CRITICAL": Fore.RED + Back.WHITE,
+        "WARNING": Fore.RED + Back.BLUE,
+        "ERROR": Fore.YELLOW + Back.BLACK,
+        "DEBUG": Fore.GREEN + Back.BLUE,
+        "INFO": Fore.GREEN
+    }
 
-def init_logger(log_path, log_level=logging.INFO):
-    logging.basicConfig(
-        filename=log_path, 
-        level=log_level,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    return logging.getLogger(__name__)
+    def format(self, record):
+        color = self.COLORS.get(record.levelname, "")
+        if color:
+            record.name = color + record.name + Fore.RESET + Back.RESET
+            record.levelname = color + record.levelname + Fore.RESET + Back.RESET
+            record.msg = color + str(record.msg) + Fore.RESET + Back.RESET 
+        return super().format(record)
+
+class CLogger(logging.Logger):
+    def __init__(self, name, log_path, log_level=logging.DEBUG):
+        super().__init__(name, log_level)
+
+        file_handler = logging.FileHandler(log_path)
+        file_formatter = CFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_formatter)
+        self.addHandler(file_handler)
+
+def init_logger(name, log_path):
+    return CLogger(name, log_path)
 
 def php_exists() -> bool:
     if platform.system() == 'Windows':
@@ -36,7 +56,6 @@ def php_exists() -> bool:
             return False
     else:
         return False 
-
 
 
 
