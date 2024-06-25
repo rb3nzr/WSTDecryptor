dd7811068b3498593c37878d5c7a1e0848bb561c
Ben <107324942+rb3nzr@users.noreply.github.com>
2024-06-25 00:12:10 -0500

    cleaned

--- webshells/Weevely3.py
+++ webshells/Weevely3.py
@@ -8,32 +8,31 @@ import zlib
 import base64 
 import datetime 
 import subprocess
-from typing import Tuple
-from colorama import Fore 
 
 class Weevely3Decryptor():
-    def __init__(self, webshell_file, pcap, ip):
+    def __init__(self, webshell_file, pcap, ip, port):
         self.webshell_file = webshell_file
         self.pcap = pcap 
         self.ip = ip 
-        self.logger = init_logger(log_path)
+        self.port = port
+        self.logger = init_logger('Weevely3Decryptor', log_path)
 
     def run(self):
-        print(Fore.GREEN + ">> Extracting payload data, this may take a minute..\n")
-        extractor = WeevelyDataExtractor(self.pcap, self.ip)
+        print(f"{cyan}>> Extracting payload data, this may take a minute..{end}")
+        extractor = WeevelyDataExtractor(self.pcap, self.ip, self.port)
         pl_data = extractor.run()
         
         k, kh, kf, p = self._get_md5_slices(self.webshell_file)
-        print(Fore.CYAN + f">> key = {k}")
-        print(Fore.MAGENTA + f">> payload prefix md5 chunk: {kh}")
-        print(Fore.MAGENTA + f">> payload suffix md5 chunk: {kf}\n")
+        print(f"\n{green}>> key => {k}{end}")
+        print(f"{green}>> payload prefix => {kh}{end}")
+        print(f"{green}>> payload suffix => {kf}{end}\n")
         
         cdt = datetime.datetime.now()
         ts = cdt.strftime("%Y%m%d_%H%M%S")
         os.rename(os.path.join(weevely_shells, "x"), os.path.join(weevely_shells, f"shell_{ts}.php"))
         output_file = os.path.join(output_path, f"weevely_output_{ts}.txt")
 
-        print(Fore.GREEN + ">> Decrypting data..\n")
+        print(f"{cyan}>> Decrypting data..{end}")
         try:
             with open(output_file, 'w') as file:
                 for i, data in pl_data:
@@ -43,13 +42,14 @@ class Weevely3Decryptor():
                             try:
                                 file.write(f"DATA: {result.decode('utf-8')}\n\n")
                             except Exception as e:
-                                self.logger.error(f">> Error writing weevely3 results: {e}\n")
+                                self.logger.critical(f">> Error writing weevely3 results: {e}")
                     except Exception as e:
-                        self.logger.error(f">> Error decrypting weevely3 data: {e}\n")
+                        self.logger.critical(f">> Error decrypting weevely3 data: {e}")
         except Exception as e:
-            self.logger.error(f">> Error opening/creating weevely output file: {e}\n")
+            self.logger.critical(f">> Error opening/creating weevely output file: {e}")
             
-        print(Fore.GREEN + f">> Done! Check output/weevely_output.txt")
+        print(f"{magenta}>> Done! Check output/weevely_output.txt{end}")
+        print(f"{magenta}>> Check wstd_log.txt for details{end}\n")
     
     def _process_data(self, data, kh, kf, k):
         match = re.search(f"{kh}(.+){kf}", data)
@@ -69,16 +69,16 @@ class Weevely3Decryptor():
 
     def _get_md5_slices(self, webshell_file):
         if not php_exists():
-            print(Fore.RED + "PHP CLI not installed, exiting..")
+            print(f"{red}>> PHP CLI not installed, exiting..{end}\n")
             sys.exit(1)
         try:
             result = subprocess.run(
                 ['php', weevely_script, webshell_file], capture_output=True, text=True)
             data = json.loads(result.stdout)
         except subprocess.CalledProcessError as e:
-            self.logger.error(f">> PHP process error: {e}\n")
+            self.logger.error(f">> PHP process error: {e}")
         except json.JSONDecodeError as e:
-            self.logger.error(f">> JSON decoding error: {e}\n")
+            self.logger.error(f">> JSON decoding error: {e}")
 
         k = data.get('k')
         kh = data.get('kh')
@@ -88,14 +88,13 @@ class Weevely3Decryptor():
         return k, kh, kf, p
     
 class WeevelyDataExtractor(BaseExtractor):
-    def __init__(self, pcap, ip):
-        super().__init__(pcap, ip)
+    def __init__(self, pcap, ip, port):
+        super().__init__(pcap, ip, port)
     
     def run(self):
         payloads = self._extract_payloads()
-        pl_data = self._extract_b64_pl_data(payloads, export=False, url_decode=False)
+        pl_data = self._extract_b64_pl_data(payloads)
         return pl_data
-
         
 
 
